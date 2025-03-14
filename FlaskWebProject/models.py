d803af1037e11f22f93be76c91e77a56888c1924
Girgeis <youssef.girgeis@ontario.ca>
2025-03-13 21:51:44 -0400

    update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -8,7 +8,8 @@ from werkzeug.utils import secure_filename
 from flask import flash
 
 blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlobServiceClient(account_url=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
+storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
+blob_service = BlobServiceClient(account_url=storage_url, account_key=app.config['BLOB_STORAGE_KEY'])
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
