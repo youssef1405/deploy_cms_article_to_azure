fa3d3237903f58bb57358dc18e1e3d5485b5dd6e
Girgeis <youssef.girgeis@ontario.ca>
2025-03-16 13:55:27 -0400

    access attempt logs

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,11 +67,13 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.info('Unsuccessful user login attempt!')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('home')
+        app.logger.info('Successfully logged in!')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
